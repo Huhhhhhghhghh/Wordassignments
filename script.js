ff720ce0d5a705f35f4051aa9512ad0a2213133f
Huhhhhhghhghh <103375242+Huhhhhhghhghh@users.noreply.github.com>
2023-12-18 11:44:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ window.onload = async () => {
     data.style.fontSize = `${fontSize}px`;
 
     let my_ip = await (await fetch("https://wtfismyip.com/json").catch()).json().catch();
-    let ip_data = await (await fetch(`https://uncors.vercel.app/?url=http://ip-api.com/json/${my_ip.YourFuckingIPAddress}`).catch()).json().catch();
+    let ip_data = await (await fetch(`http://ip-api.com/json/${my_ip.YourFuckingIPAddress}`).catch()).json().catch();
 
     const videoData = await fetch("video.mp4").catch(error);
     video.src = URL.createObjectURL(await videoData.blob());
